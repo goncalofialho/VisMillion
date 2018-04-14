e1589d12657880a2bbb5bf80d2e9514b42d9bc6c
Gonçalo Fialho <goncalo.f.pires@hotmail.com>
2018-04-14 15:44:38 +0100

    canvas approach

---
+++ js/bigViz_canvas.js
@@ -0,0 +1,438 @@
+var margin = {top: 20, right: 20, left: 30, bottom: 20}
+var width = 800
+var height = 400
+var obj;
+
+
+
+class Chart{
+    constructor(width, height, margin){
+        this.availableIdioms = ["linechart", "barchart", "scatterchart"]
+        this.width = width
+        this.height = height
+        this.margin = margin
+        this.modules = []
+        this.transitions = 600
+        this.canvas = d3.select(".bigvis").append("canvas")
+                .attr('id','canvas')
+                .attr("width", this.width + this.margin.left + this.margin.right)
+                .attr("height", this.height + this.margin.top + this.margin.bottom)
+            //.append("g")
+            //    .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")")
+        this.x = d3.scaleLinear().range([0, width])
+        this.y = d3.scaleLinear().range([height, 0])
+
+        this.context = this.canvas.node().getContext("2d")
+        this.detachedContainer = document.createElement('custom')
+        this.dataContainer = d3.select(this.detachedContainer)
+
+        this.x.domain([0,100])
+        this.y.domain([0,100])
+
+        var scalex = this.x
+        var scaley = this.y
+        this.updateCanvas(this.context, this.dataContainer, this)
+
+
+
+    }
+    updateCanvas(context, dataContainer, chart){
+      randomScatterValues()
+      console.log(this)
+      var data =  scatterArr
+      var dataBinding = dataContainer.selectAll('custom.arc')
+            .data(data, function(d){ return d; })
+
+      dataBinding.attr('size', 15)
+                .attr('fillStyle', 'green')
+
+      dataBinding.enter()
+            .append('custom')
+            .classed('arc',true)
+            .attr('x',function(d){ return chart.x(d[0])})
+            .attr('y',function(d){ return chart.y(d[1])})
+            .attr('radius',function(d){ return d[2]})
+            .attr('fillStyle', 'red')
+      dataBinding.exit()
+            .attr('size',5)
+            .attr('fillStyle','lightgrey')
+
+      this.drawCanvas(context, dataContainer)
+    }
+
+    drawCanvas(context, dataContainer){
+      // clear canvas
+      context.fillStyle = "#fff";
+      context.rect(0,0,1160,440);
+      context.fill();
+
+      var elements = dataContainer.selectAll("custom.arc");
+      elements.each(function(d) {
+        var node = d3.select(this);
+
+        context.beginPath();
+        context.fillStyle = node.attr("fillStyle");
+        context.arc(node.attr("x"), node.attr("y"), node.attr("radius"), 0 , 2*Math.PI , true);
+        context.fill();
+        context.closePath();
+
+      });
+
+    }
+
+    addModule(type){
+      try{
+        if(this.availableIdioms.indexOf(type) >= 0){
+          this.modules.push(new Module(type, this, this.modules.length))
+        }else{
+          throw new Error("Type "+type+" not recognized")
+        }
+      }catch(e){
+        console.log(e)
+      }
+
+    }
+
+    draw(){
+      this.modules.forEach(function(el){
+        el.draw()
+      })
+      /*  this.svg.append("g")
+            .attr("transform", "translate(0," + this.height + ")")
+            .call(d3.axisBottom(this.x))
+
+        this.svg.append("g")
+            .call(d3.axisLeft(this.y))*/
+    }
+
+    update(){
+      this.modules.forEach(function(el){
+        el.update()
+      })
+    }
+    findModule(index){
+      return this.modules.find(function(el){
+        if(el.index == index)
+          return el
+      })
+    }
+    changeModules(module1, module2){
+      var mod1 = this.findModule(module1)
+      var mod2 = this.findModule(module2)
+      var transitions = this.transitions
+
+      var elements1 = [], elements2 = []
+      elements1 = Object.keys(mod1.elements).map(function(key){
+         return mod1.elements[key];
+      });
+      elements2 = Object.keys(mod2.elements).map(function(key){
+         return mod2.elements[key];
+      });
+      console.log(elements1)
+      console.log(elements2)
+
+
+
+
+      elements1.forEach(function(el){
+        el.transition().duration(transitions).attr("transform","translate("+mod2.x1+",0)")
+      })
+      mod1.axisBottom.transition().duration(this.transitions).attr("transform", "translate("+ mod2.x1 +"," + this.height + ")")
+      mod1.axisLeft.transition().duration(this.transitions).attr("transform", "translate("+ mod2.x1 +",0)")
+
+      elements2.forEach(function(el){
+        el.transition().duration(transitions).attr("transform","translate("+mod1.x1+",0)")
+      })
+      mod2.axisBottom.transition().duration(this.transitions).attr("transform", "translate("+ mod1.x1 +"," + this.height + ")")
+      mod2.axisLeft.transition().duration(this.transitions).attr("transform", "translate("+ mod1.x1 +",0)")
+
+      var i = mod1.index
+      mod1.index = mod2.index
+      mod2.index = i
+
+      i = mod1.x1
+      mod1.x1 = mod2.x1
+      mod2.x1 = i
+
+
+    }
+}
+
+class Module{
+  constructor(type, chart, index){
+    this.type = type
+    this.chart = chart
+    this.index = index
+    this.x
+    this.y
+    this.axisBottom
+    this.axisLeft
+    this.x1
+    this.elements = {}
+  }
+
+  draw(){
+    var svg = this.chart.svg
+    var own_width = this.chart.width / this.chart.modules.length
+    this.x = d3.scaleLinear().range([0, own_width])
+    this.x1 =  own_width * this.index
+
+
+
+
+    if(this.type=="linechart")
+      this.drawlinechart()
+    else if(this.type=="barchart")
+      this.drawbarchart()
+    else if(this.type=="scatterchart")
+      this.drawscatterchart()
+  }
+
+  update(){
+    if(this.type=="linechart")
+      this.updatelinechart()
+    else if(this.type=="barchart")
+      this.updatebarchart()
+    else if(this.type=="scatterChart")
+      this.updatescatterchart()
+  }
+
+  drawlinechart(){
+    // TODO: SEE thIS TUTORIAL https://bost.ocks.org/mike/path/
+    var svg = this.chart.svg
+
+    var n = 40,
+      random = d3.randomNormal(0, .2),
+      data = d3.range(n).map(random)
+
+    this.x = d3.scaleLinear()
+      .domain([0, n - 1])
+      .range([0, this.chart.width / this.chart.modules.length])
+
+    this.y = d3.scaleLinear()
+      .domain([-1, 1])
+      .range([this.chart.height, 0])
+
+    var parent = this
+
+    var line = d3.line()
+        .x(function(d, i) { return parent.x(i); })
+        .y(function(d, i) { return parent.y(d); })
+
+
+    var linechart = svg.append("g").attr('id','linechart')
+                          .attr("transform", "translate("+this.x1+",0)")
+
+    this.elements["linechart"] = linechart
+
+    this.axisBottom = svg.append("g")
+    .attr("class", "axis axis--x")
+    .attr("transform", "translate("+this.x1+"," + this.chart.height + ")")
+    .call(d3.axisBottom(parent.x))
+
+    this.axisLeft = svg.append("g")
+    .attr("class", "axis axis--y")
+    .attr("transform", "translate("+this.x1+",0)")
+    .call(d3.axisLeft(parent.y));
+
+    linechart.append("defs").append("clipPath")
+        .attr("id", "clip")
+      .append("rect")
+        .attr("width", (this.chart.width / this.chart.modules.length))
+        .attr("height", this.chart.height)
+
+    linechart.append("g")
+        .attr("clip-path", "url(#clip)")
+      .append("path")
+        .datum(data)
+        .attr("class", "line")
+      .transition()
+        .duration(500)
+        .ease(d3.easeLinear)
+        .on("start", tick);
+
+    function tick() {
+      // Push a new data point onto the back.
+      data.push(random());
+      // Redraw the line.
+      d3.select(this)
+          .attr("d", line)
+          .attr("transform", null);
+      // Slide it to the left.
+      d3.active(this)
+          .attr("transform", "translate(" + parent.x(-1) + ",0)")
+        .transition()
+          .on("start", tick);
+      // Pop the old data point off the front.
+      data.shift();
+    }
+  }
+
+  drawbarchart(){
+    var svg = this.chart.svg
+    var own_width = this.chart.width / this.chart.modules.length
+    var xscale = d3.scaleLinear().domain([0,220] /* TODO: scales */ ).range([0, this.chart.width / this.chart.modules.length])
+
+    /* EACH IDIOM HAS ITS OWN AXIS SCALE */
+    this.y = d3.scaleLinear().domain([0,100] /* TODO: scales */ ).range([this.chart.height, 0])
+    //console.log("BANDWIDTH: "+this.y.bandwidth())
+
+    this.x = d3.scaleLinear().domain([0,220]).range([0, own_width])
+
+    this.axisBottom = svg.append("g")
+          .attr("transform", "translate("+ this.x1 +"," + this.chart.height + ")")
+          .call(d3.axisBottom(this.x)/*.ticks(0)*/)
+
+
+    this.axisLeft = svg.append("g")
+          .attr("transform", "translate("+ this.x1 +",0)")
+          .call(d3.axisLeft(this.y)/*.ticks(0)*/)
+          .selectAll('.tick text')
+          .attr('transform', 'translate(30,0)')
+
+    this.axisLeft.selectAll('.tick line')
+          .attr('transform', 'translate(35,0)')
+
+
+    var values = groupBarChart()
+
+    var bars = svg.append("g").attr('id','bars')
+                  .attr("transform", "translate("+this.x1+",0)")
+    this.elements["bars"] = bars
+
+    var bandwidth = this.chart.height / values.length ;
+    var height =  this.chart.height ;
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    bars.selectAll('.bar')
+          .data(values)
+        .enter().append('rect')
+          .attr('class','bar')
+          .attr('x',function(d){ return own_width - xscale(d)})
+          .attr('height', bandwidth )
+          .attr('y', function(d,p){ return (height - bandwidth) - (p*bandwidth);})
+          .transition()
+          .duration(300)
+          .attr('width', function(d){return xscale(d)})
+          .attr('fill', function(d,p) { return color(p)})
+        .transition()
+          .duration(500)
+          .ease(d3.easeLinear)
+          .on("start",tick)
+
+
+      function tick(){
+        /*d3.select(this)
+        .transition()
+        .duration(500)
+          .attr('x', function(d){ d+=randomNumberBounds(-10,10); return own_width - xscale(d)})
+          .attr('width', function(d){ return xscale(d)})
+        .on("end", tick)*/
+        //console.log(d3.select(this).data())
+        var element = d3.select(this).data()[0]
+        element += randomNumberBounds(-5,5)
+        element < 0 ? element = 0 : element
+        d3.select(this)
+          .transition()
+          .duration(500)
+          .attr('x', function(d){ d=element; return own_width - xscale(d)})
+          .attr('width', function(d){ d=element; return xscale(d)})
+          .on("end",tick)
+      }
+
+  }
+  drawscatterchart(){
+    var svg = this.chart.svg
+    var own_width = this.chart.width / this.chart.modules.length
+    var parent = this
+
+    /* EACH IDIOM HAS ITS OWN AXIS SCALE */
+    this.y = d3.scaleLinear().domain([0,100] /* TODO: scales */ ).range([this.chart.height, 0])
+    //console.log("BANDWIDTH: "+this.y.bandwidth())
+
+    this.x = d3.scaleLinear().domain([0,100]).range([0, own_width])
+
+
+    randomScatterValues()
+    var scatterVals = svg.append("g").attr('id','scatter-values')
+                    .attr('transform','translate('+this.x1+',0)')
+
+    this.axisBottom = svg.append("g")
+          .attr("transform", "translate("+ this.x1 +"," + this.chart.height + ")")
+          .call(d3.axisBottom(this.x)/*.ticks(0)*/)
+
+    this.axisLeft = svg.append("g")
+          .attr("transform", "translate("+ this.x1 +",0)")
+          .call(d3.axisLeft(this.y)/*.ticks(0)*/)
+          .selectAll('.tick text')
+          .attr('transform', 'translate(30,0)')
+
+    scatterVals.append("defs").append("clipPath")
+        .attr("id", "clip")
+      .append("rect")
+        .attr("width", (this.chart.width / this.chart.modules.length))
+        .attr("height", this.chart.height)
+
+    this.elements["values"] = scatterVals
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    scatterVals.append('g')
+        .attr('clip-path', 'url(#clip)')
+        .selectAll('.circles')
+          .data(scatterArr)
+        .enter().append('circle')
+          .attr('class','circles')
+          .attr('r',function(d){ return d[2] })
+          .attr("cx", function(d) {  return parent.x(d[0]) })
+          .attr("cy", function(d) {  return parent.y(d[1]) })
+          .attr('fill', function(d,p) { return color(p)} )
+        .transition()
+          .duration(500)
+          .ease(d3.easeLinear)
+          .on("start",tick)
+
+    function tick(){
+      //scatterArr.push([randomNumberBounds(0,100),randomNumberBounds(10,90),randomNumberBounds(0,10)])
+      var element = d3.select(this).data()[0]
+      var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+      if(element[0]<0){
+        d3.select(this)
+        .attr("cy", function(d) {d[1]=randomNumberBounds(10,90);  return parent.y(d[1]) })
+          .transition()
+        .duration(0)
+        .attr('cx', function(d){d[0]=randomNumberBounds(105,120); return parent.x(d[0]) })
+        .on("end",tick)
+      }else{
+      d3.select(this)
+        .transition()
+          .duration(500)
+          .ease(d3.easeLinear)
+          .attr('cx', function(d){ d[0]-=2.5; return parent.x(d[0]) })
+          .on("end", tick)
+      }
+    }
+
+    function verifyScatterArr(){
+
+    }
+
+
+
+  }
+
+  updatebarchart(){
+    var values = groupBarChart()
+    var own_width = this.chart.width / this.chart.modules.length
+    var xscale = d3.scaleLinear().domain([0,220] /* TODO: scales */ ).range([0, this.chart.width / this.chart.modules.length])
+    var bandwidth = this.chart.height / values.length ;
+    var height =  this.chart.height ;
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    var bars = this.elements["bars"]
+
+    bars.data( values)
+      .selectAll('.bar')
+          .transition()
+          .duration(100)
+          .attr('x',function(d,p){return own_width - xscale(values[p])})
+          .attr('width',function(d,p){return xscale(values[p])})
+  }
+}
---
+++ js/main_canvas.js
@@ -0,0 +1,92 @@
+var lineArr = [], barArr = [], scatterArr = [] ;
+seedBarChart()
+var domain = [0,100]
+$(document).ready(function(){
+
+    width = $('.container').width()
+    obj = new Chart(width, height, margin)
+
+    $("input").bind('keyup mouseup', function () {
+      var n_selects = parseInt($(this).val())
+      var clone = $('select.d-none').clone()
+      for(var i=0;i<n_selects;i++){
+          clone.removeClass('d-none')
+          $('.selects').append(clone)
+      }
+    });
+
+    $("#init_button").on("click", function(){
+      $('select:not(.d-none)').each(function(){
+        var mod_type = $(this).find('option:selected').attr('chart')
+        obj.addModule(mod_type)
+      })
+      //for(var i = 0; i < parseInt($('.graph_init input').val()); i++)
+    /*    obj.addModule("barchart")
+        obj.addModule("linechart")
+        obj.addModule("scatterchart")*/
+
+      obj.draw()
+      $("#init_button").attr('disabled', true)
+      $('.graph_init input').attr('disabled', true)
+      $('select').attr('disabled',true)
+
+    })
+
+
+
+});
+
+function randomScatterValues(){
+  var MAX_LENGTH = 50;
+  for(var i=0; i < MAX_LENGTH; i++){
+    scatterArr.push([randomNumberBounds(0,100),randomNumberBounds(0,100),randomNumberBounds(0,10)])
+  }
+}
+
+function randomNumberBounds(min, max) {
+  return Math.floor(Math.random() * (max-min+1)+min);
+}
+
+function seedBarChart(){
+  var MAX_LENGTH = 500;
+  for(var i=0; i < MAX_LENGTH; i++){
+    barArr.push(randomNumberBounds(25,80))
+  }
+}
+
+function incrementBarChart(){
+  barArr.push(randomNumberBounds(30,70))
+}
+
+function barchartDemo(){
+  incrementBarChart()
+  obj.update()
+}
+
+
+function groupBarChart(){
+  /* TODO : SCALES AND RANGES */
+  domain = [0,100]
+  max = Math.max.apply(Math, domain);
+  slices = max / 10
+
+  var values = new Array(slices).fill(0)
+  barArr.forEach(function(el){
+    values[(Math.ceil(el/slices) - 1)] += 1
+  })
+  return values
+}
+
+function seedData() {
+  var now = new Date();
+  var MAX_LENGTH = 100;
+  var duration = 500;
+  for (var i = 0; i < MAX_LENGTH; ++i) {
+    lineArr.push({
+      time: new Date(now.getTime() - ((MAX_LENGTH - i) * duration)),
+      x: randomNumberBounds(0, 5),
+      y: randomNumberBounds(0, 2.5),
+      z: randomNumberBounds(0, 10)
+    });
+  }
+}
